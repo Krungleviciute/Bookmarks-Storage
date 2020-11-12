be8430d193ae8fa250311e751961e65b155c2d04
Krungleviciute <d.krungleviciute@gmail.com>
2020-11-12 16:52:01 -0600

    Bookmarks Keeper App Added

---
+++ script.js
@@ -0,0 +1,136 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+let bookmarks = [];
+
+//Show Modal, Focus on Input
+const showModal = () => {
+    modal.classList.add('show-modal');
+    websiteNameEl.focus();
+}
+
+const storeBookmark = (e) => {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+
+    if(!urlValue.includes('http://', 'https://')){
+        urlValue = `https://${urlValue}`;
+    }
+
+    if(!validateURL(nameValue, urlValue)){
+        return false;
+    }
+
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    };
+
+    bookmarks.push(bookmark)
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
+    
+}
+
+const validateURL = (nameValue, urlValue) => {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g
+    const regex = new RegExp(expression);
+    if(!nameValue || !urlValue){
+        alert('please submit values for both fields');
+        return false
+
+    }
+    if(urlValue.match(regex)){
+        return true;
+    }
+    if(!urlValue.match(regex)){
+        alert('please provide a valid website')
+        return false;
+    }
+
+    return true;
+}
+
+const createBookmark = () => {
+
+    // remove all bookmark elements
+    bookmarksContainer.textContent = "";
+
+    bookmarks.forEach((bookmark) => {
+        const { name, url } = bookmark;
+
+        const item = document.createElement('div');
+        item.classList.add('item');
+
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('far', 'fa-times-circle');
+        closeIcon.setAttribute('title', "Delete Bookmark");
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+        closeIcon.setAttribute('id', 'delete-bookmark');
+
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name');
+
+        const favicon = document.createElement('img');
+        favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+        favicon.setAttribute('alt', `${name} Favicon`);
+
+        const link = document.createElement('a');
+        link.setAttribute('href', `${url}`);
+        link.setAttribute('target', '_blank');
+        link.textContent = name;
+
+        //Append to bookmarks container:
+        linkInfo.append(favicon, link);
+        item.append(closeIcon, linkInfo);
+        bookmarksContainer.appendChild(item);
+    })
+}
+
+const fetchBookmarks = () => {
+    if(localStorage.getItem('bookmarks')){
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else{
+        bookmarks = [
+            {
+                name: "Dalia's GitHub",
+                url: "https://github.com/Krungleviciute"
+            }
+        ];
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+    }
+    createBookmark();
+}
+
+const deleteBookmark = (url) => {
+    bookmarks.forEach((bookmark, index) => {
+        if(bookmark.url === url){
+            bookmarks.splice(index, 1);
+        }
+    });
+
+    // update array in Local Storage and re-populate the DOM
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+}
+
+modalShow.addEventListener('click', showModal)
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('show-modal')
+})
+window.addEventListener('click', (e) => {
+    e.target === modal ? modal.classList.remove('show-modal') : false;
+});
+
+bookmarkForm.addEventListener('submit', storeBookmark)
+
+// On Load
+fetchBookmarks();
